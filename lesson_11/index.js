8e4091d234908da4706dbdf4a3a15d7f75cc34a3
Ivan Dmytrenko <lucobrazipl@gmail.com>
2018-05-07 22:20:45 +0300

    Calculator has been added

---
+++ lesson_11/index.js
@@ -0,0 +1,35 @@
+let a = 1;
+let b = 13;
+let operation = "sub";
+
+switch(operation) {
+	case "add":
+	console.log(a + b);
+	document.write(a + b);
+	break;
+
+	case "sub":
+	console.log(a - b);
+	document.write(a - b);
+	break;
+
+	case "mult":
+	console.log(a * b);
+	document.write(a * b);
+	break;
+
+	case "div":
+	console.log(a / b);
+	document.write(a / b);
+	break;
+
+	case "pow":
+	console.log(Math.pow(a,b));
+	document.write(Math.pow(a,b));
+	break;
+
+	default:
+	console.log("Значение не соответствует операторам (add || sub || mult || div || pow) ");
+	break;
+
+}
