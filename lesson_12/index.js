9b72a148f0b3cd5d9ed520097ed19ad49641c15b
Ivan Dmytrenko <lucobrazipl@gmail.com>
2018-05-14 22:42:26 +0300

    Function

--- lesson_12/index.js
+++ lesson_12/index.js
@@ -1,5 +1,5 @@
 const arr1 = [1, 13, -3, 0];
-let sum = 0;
+let sum = 0;in
 let min = arr1[0];
 let max = arr1[0];
 
@@ -11,3 +11,24 @@ console.log(sum);
 console.log(sum / arr1.length);
 console.log( Math.min.apply(null, arr1));
 console.log( Math.max.apply(null, arr1));
+
+
+const obj1 = { from: 1, to: 4, value: [4, 5, 12, 7, 5, 4], ignore: [2] };
+
+console.log(obj1.join(' ,'));
+
+
+let  arr = [4, 5, 12, 7, 5, 4]; 
+let  newArr = [];
+
+    for (let  i = 0; i < arr.length; i++) {
+        if (typeof arr[i] != "string") {
+            continue;
+        } else {
+            newArr.push(arr[i]); 
+        }
+        }
+
+alert(newArr);
+
+
