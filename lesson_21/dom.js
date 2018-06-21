eb8138124539e8303f73e2ca2a9edcf9271ad0f9
Ivan Dmytrenko <lucobrazipl@gmail.com>
2018-06-21 21:57:50 +0300

    My coordinate

---
+++ lesson_21/dom.js
@@ -0,0 +1,26 @@
+export default class Dom {
+    constructor() {
+        this.lat = document.getElementById('lat');
+        this.long = document.getElementById('long');
+        this.city = document.getElementById('city');
+        this.country = document.getElementById('country');
+        this.ip = document.getElementById('ip');
+        this.id = document.getElementById('gif');
+    }
+
+    showPreloader() {
+        this.id.style.opacity = 0;
+    };
+
+    hidePreloader() {
+        this.id.style.opacity = 1;
+    };
+
+    setCoordinates(crd) {
+        this.lat.value = crd.latitude;
+        this.long.value = crd.longitude;
+        this.city.value = crd.city;
+        this.country.value = crd.country_name;
+        this.ip.innerHTML = `ip: ${crd.ip}`;
+    };
+}
