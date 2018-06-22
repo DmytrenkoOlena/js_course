a2be8fc17a7ea51c26237c36329dbb29b93c6cba
Ivan Dmytrenko <lucobrazipl@gmail.com>
2018-06-22 22:06:16 +0300

    module3

---
+++ module_3/index.js
@@ -0,0 +1,22 @@
+
+import UIController from "./ui_controller.js";
+
+// export default class UIController {
+//     constructor() {
+//       // this.regExp = /^\d{4}\-\d{4}\-\d{4}\-\d{4}$/;
+//       this.card = document.getElementById('card');
+//       this.card.onchange = this.validCard.bind(this);
+//       this.mask = '0000-0000-0000-0000';
+//   }
+//   validCard() {
+//       if (!this.mask.test(this.card.value)) {
+//           this.card.style.border = "2px solid red";
+//       } else {
+//           this.card.style.border = "2px solid green";
+//       }
+//   }    
+// }
+
+// const uiController = new UIController();
+
+
