36d1f48b616a19db5d94061797b27c4c5dc7eb5b
Ivan Dmytrenko <lucobrazipl@gmail.com>
2018-06-22 18:53:06 +0300

    Form Validation

--- lesson_24/index.js
+++ lesson_24/index.js
@@ -6,10 +6,49 @@ class Phone {
 	}
 	validPhone() {
 		if (!this.regExp.test(this.phone.value)) {
-			this.phone.style.border = '2px solid red';
+			this.phone.style.border = "2px solid red";
 		} else {
-			this.phone.style.border = '2px solid green';
+			this.phone.style.border = "2px solid green";
 		}
 	}
 }
+class Email {
+    constructor() {
+        this.regExp = /^\w+\.?[\w]+@\w{1,12}\.[a-zA-Z]{2,6}/;
+        this.email = document.getElementById('email');
+        this.email.onchange = this.validEmail.bind(this);
+    }
+    validEmail() {
+        if(!this.regExp.test(this.email.value)) {
+            this.email.style.border = "2px solid red";
+        } else {
+            this.email.style.border = "2px solid green";
+        }
+    }
+}
+class TextArea {
+	constructor () {
+		this.regExp = /\w[\w!#$%&'*+\-/=?^_`{|}~]+\.?[\w!#$%&'*+\-/=?^_`{|}~]+@\w{1,12}\.[A-z]{2,3}/igm;
+		this.content = document.getElementById('content');
+		this.search = document.getElementById('search');
+		
+		this.search.onclick = this.getEmails.bind(this);
+	}
+	getEmails() {
+		this.emailList = this.content.value.match(this.regExp);
+		if(this.emailList) {
+			this.list = document.createElement('ol');
+			this.emailList.forEach(el => {
+				let li = document.createElement('li');
+				li.id = el;
+				li.textContent = el;
+				this.list.appendChild(li);
+			});
+			document.body.appendChild(this.list);
+		}
+	}
+}
+
 const phone = new Phone();
+const email = new Email();
+const textArea = new TextArea();
